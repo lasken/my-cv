4e6629ae98a623a3ebc8a5bcfced7068aceb612e
L-kenny <128179042+lasken@users.noreply.github.com>
2023-12-22 15:45:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('open');
+}
+function sendEmail() {
+  window.location.href = "mailto:lasisikenny7@icloud.com";
+}
